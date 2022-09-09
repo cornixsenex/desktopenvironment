a2d039933abdead0edcb8ad483f046f0e63403e4
cornixsenex <phallusferrus@toke.com>
2022-09-09 10:07:22 -0700

    added haze to dwmweather override

--- dwmscripts/dwmtemp.sh
+++ dwmscripts/dwmtemp.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-rtemp=$(sensors | awk 'NR==21{print $2}')
+rtemp=$(sensors | awk 'NR==26{print $2}')
 temp="${rtemp//[+]/}"
+#rgtemp=$(sensors | awk 'NR==5{print $2}')
+#rgtemp=$(sensors | awk 'NR==12{print $2}')
+#rgtemp=$(sensors | awk 'NR==16{print $2}')
+gtemp="${rgtemp//[+]/}"
 icon=
 
-printf " %s%s \\n" "$icon " "$temp"
+printf " %s %s \\n" "$icon" "$temp"
+
+#printf " %s%s  %s %s \\n" "$icon " "$temp" "$icon" "$gtemp"
